9dcd9eaca9f768d7afbe87db2b721f5cfec91c3b
nhiquach <nhiquach@berkeley.edu>
2015-04-16 22:20:09 -0700

    fixed nav bar, images on show page

--- personalProject/app/models/post.rb
+++ personalProject/app/models/post.rb
@@ -2,12 +2,16 @@
 #
 # Table name: posts
 #
-#  id          :integer          not null, primary key
-#  title       :string(255)
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
-#  user_id     :integer
+#  id                 :integer          not null, primary key
+#  title              :string(255)
+#  description        :text
+#  created_at         :datetime
+#  updated_at         :datetime
+#  user_id            :integer
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Post < ActiveRecord::Base
