5375dd531a4238301194da7125375df01b15d749
nhiquach <nhiquach@berkeley.edu>
2015-04-27 21:11:28 -0700

    merge stuff

--- personalProject/config/routes.rb
+++ personalProject/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	
   get 'tags/:tag', to: 'posts#index', as: :tag
   devise_for :users
   resources :posts
@@ -6,5 +7,4 @@ Rails.application.routes.draw do
   resources :tag
   root "posts#index"
 
-  
 end
