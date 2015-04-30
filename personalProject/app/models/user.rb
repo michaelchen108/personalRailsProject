82f0a7d127469655e154085bb5fbf338dd505c19
nhiquach <nhiquach@berkeley.edu>
2015-04-29 20:29:10 -0700

    comments

--- personalProject/app/models/user.rb
+++ personalProject/app/models/user.rb
@@ -2,31 +2,24 @@
 #
 # Table name: users
 #
-#  id                          :integer          not null, primary key
-#  email                       :string(255)      default(""), not null
-#  encrypted_password          :string(255)      default(""), not null
-#  reset_password_token        :string(255)
-#  reset_password_sent_at      :datetime
-#  remember_created_at         :datetime
-#  sign_in_count               :integer          default(0), not null
-#  current_sign_in_at          :datetime
-#  last_sign_in_at             :datetime
-#  current_sign_in_ip          :string(255)
-#  last_sign_in_ip             :string(255)
-#  created_at                  :datetime
-#  updated_at                  :datetime
-#  name                        :string(255)
-#  admin                       :boolean
-#  address                     :string(255)
-#  latitude                    :float
-#  longitude                   :float
-#  my_draft_comments_count     :integer          default(0)
-#  my_published_comments_count :integer          default(0)
-#  my_comments_count           :integer          default(0)
-#  draft_comcoms_count         :integer          default(0)
-#  published_comcoms_count     :integer          default(0)
-#  deleted_comcoms_count       :integer          default(0)
-#  spam_comcoms_count          :integer          default(0)
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  name                   :string(255)
+#  admin                  :boolean
+#  address                :string(255)
+#  latitude               :float
+#  longitude              :float
 #
 
 class User < ActiveRecord::Base
@@ -37,6 +30,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+  
+  acts_as_commontator
 
   validates :name, presence: :true
   validates :address, presence: :true
--- personalProject/config/routes.rb
+++ personalProject/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :tag
-  
+  mount Commontator::Engine => '/commontator'
   root "posts#index"
 
 end
